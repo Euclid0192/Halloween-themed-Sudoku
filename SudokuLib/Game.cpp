50f7349b223fcf8a993ff6ec9eb229248d4be5c9
YaxuanTang <Tangyaxuan_mandy@outlook.com>
2023-10-24 15:35:22 -0400

    Instruction content drawn, only for level 1, still need to fix scoreboard and intro page when changing different level.

--- SudokuLib/Game.cpp
+++ SudokuLib/Game.cpp
@@ -294,10 +294,34 @@ void Game::DrawIntroPage(std::shared_ptr<wxGraphicsContext> graphics){
     //
     // Draw a filled rectangle
     //
-    wxBrush rectBrush(*wxCYAN);
+    wxBrush rectBrush(*wxWHITE);
     graphics->SetBrush(rectBrush);
-    graphics->SetPen(*wxTRANSPARENT_PEN);
-    graphics->DrawRectangle(150, 198, 700, 350);
+    graphics->SetPen(*wxBLACK);
+    graphics->DrawRectangle(150, 170, 600, 350);
+
+
+    wxFont bigFont(wxSize(0, 80),
+                   wxFONTFAMILY_SWISS,
+                   wxFONTSTYLE_NORMAL,
+                   wxFONTWEIGHT_BOLD);
+    graphics->SetFont(bigFont, wxColour(0, 500, 0));
+
+    double wid, hit;
+    graphics->GetTextExtent(L"Centered Text", &wid, &hit);
+    graphics->DrawText(L"Level 1 Begin", 200,200);
+
+    wxFont smallFont(wxSize(0, 40),
+                   wxFONTFAMILY_SWISS,
+                   wxFONTSTYLE_NORMAL,
+                   wxFONTWEIGHT_BOLD);
+    graphics->SetFont(smallFont, wxColour(0,0,500));
+
+    graphics->GetTextExtent(L"Centered Text", &wid, &hit);
+    graphics->DrawText(L"space: Eat", 320,300);
+    graphics->DrawText(L"0-8: Regurgitate", 320,375);
+    graphics->DrawText(L"B: Headbutt", 320,450);
+
+
 
 }
 
