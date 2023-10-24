c4f8f88a6b2af5bbff5d018e8b6fc235993f7eeb
YaxuanTang <Tangyaxuan_mandy@outlook.com>
2023-10-24 15:14:27 -0400

    Adding a test for scoreboard (only constructor function)

---
+++ Tests/ScoreBoardTest.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file ScoreBoardTest.cpp
+ * @author yaxuan
+ * Tests for scoreboard
+ */
+
+#include <pch.h>
+#include "gtest/gtest.h"
+#include <ScoreBoard.h>
+
+using namespace std;
+
+/**
+ * Constructor test
+ */
+TEST(ScoreBoardTest, Construct){
+    ScoreBoard scoreBoard ;
+}
